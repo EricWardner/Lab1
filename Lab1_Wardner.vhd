e1fafa046d8679a1b924579717a1942acf61bba5
unknown <C16Eric.Wardner@C16EJWardner.usafa.ds.af.edu>
2014-01-27 11:06:56 -0700

    added 8-bit functionality

--- Lab1_Wardner.vhd
+++ Lab1_Wardner.vhd
@@ -19,6 +19,7 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -30,31 +31,40 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity Lab1_Wardner is
-    Port ( A : in  STD_LOGIC;
-           B : in  STD_LOGIC;
-           C : in  STD_LOGIC;
-           X : out  STD_LOGIC;
-           Y : out  STD_LOGIC;
-           Z : out  STD_LOGIC);
+
+   Port ( 
+	        --Inputs
+           A : in std_logic_vector (7 downto 0);
+           --Outputs
+           X : out std_logic_vector (7 downto 0));
+			  
+--				A : in  STD_LOGIC;
+--           B : in  STD_LOGIC;
+--           C : in  STD_LOGIC;
+--           X : out  STD_LOGIC;
+--           Y : out  STD_LOGIC;
+--           Z : out  STD_LOGIC);
 end Lab1_Wardner;
 
 architecture Behavioral of Lab1_Wardner is
 
-signal A_NOT, B_NOT, C_NOT, ABnotCnot, AnotB, AnotC, BCnot, BnotC : STD_LOGIC;
+--signal A_NOT, B_NOT, C_NOT, ABnotCnot, AnotB, AnotC, BCnot, BnotC : STD_LOGIC;
 
 begin
 
-A_NOT <= not A;
-B_NOT <= not B;
-C_NOT <= not C;
-ABnotCnot <= A and B_NOT and C_NOT;
-AnotB <= A_NOT and B;
-AnotC <= A_NOT and C;
-BCnot <= B and C_NOT;
-BnotC <= B_NOT and C;
-X <= ABnotCnot or AnotB or AnotC;
-Y <= BCnot or BnotC;
-Z <= C;
+X <= std_logic_vector(UNSIGNED(not(A)) + 1);
+
+--A_NOT <= not A;
+--B_NOT <= not B;
+--C_NOT <= not C;
+--ABnotCnot <= A and B_NOT and C_NOT;
+--AnotB <= A_NOT and B;
+--AnotC <= A_NOT and C;
+--BCnot <= B and C_NOT;
+--BnotC <= B_NOT and C;
+--X <= ABnotCnot or AnotB or AnotC;
+--Y <= BCnot or BnotC;
+--Z <= C;
 
 end Behavioral;
 
